7938f53780880440e22905e0c0baadbadccfc29e
Patrick Lin <linpatrick11@gmail.com>
2022-06-23 00:31:46 -0400

    added arrayutils to template and 797 e

--- java/src/cf/contest/round797/E.java
+++ java/src/cf/contest/round797/E.java
@@ -1,29 +1,169 @@
 package cf.contest.round797;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class E {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        FastReader sc = new FastReader();
+        MathUtils mathUtils = new MathUtils();
+        ArrayUtils arrayUtils = new ArrayUtils();
         int t = sc.nextInt();
-        for(int tests = 0; tests < t; tests++) {
+        for (int _i = 0; _i < t; _i++) {
+            // n must be even
             int n = sc.nextInt();
             int k = sc.nextInt();
-            int[] a = new int[n];
-            int[] mod = new int[n];
-            List<int[]> list = new ArrayList<>();
-            for(int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-                list.add(new int[]{a[i], a[i] % k});
-            }
+            int[] a = sc.readIntArray(n);
 
-            list.sort(Comparator.comparingInt(o -> o[1]));
+            long sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += a[i] / k;
+                a[i] %= k;
+            }
 
             Arrays.sort(a);
-
-            for(int i = 0; i < n; i++) {
-
+            a = ArrayUtils.reverse(a);
+            for (int i = 0, j = n - 1; i < j; i++, j--) {
+                while(a[i] + a[j] < k && i < j) {
+                    j--;
+                }
+                if(i == j) {
+                    break;
+                }
+                sum++;
             }
+
+            System.out.println(sum);
+
+        }
+    }
+
+    static class FastReader {
+        /**
+         * Uses BufferedReader and StringTokenizer for quick java I/O
+         * get next int, long, double, string
+         * get int, long, double, string arrays, both primitive and wrapped object when array contains all elements
+         * on one line, and we know the array size (n)
+         * next: gets next space separated item
+         * nextLine: returns entire line as space
+         */
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public String[] readStringArrayLine(int n) {
+            String line = "";
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return line.split(" ");
+        }
+
+        public String[] readStringArrayLines(int n) {
+            String[] result = new String[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.next();
+            }
+            return result;
+        }
+
+        // to parse something else:
+        // T x = T.parseT(fastReader.next());
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] readIntArray(int n) {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextInt();
+            }
+            return result;
+        }
+
+        public Integer[] readIntArrayObject(int n) {
+            Integer[] result = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextInt();
+            }
+            return result;
+        }
+
+        public char[] readCharArray(int n) {
+            return this.nextLine().toCharArray();
+        }
+
+    }
+
+    static class MathUtils {
+        public MathUtils() {
+        }
+
+        public long gcdLong(long a, long b) {
+            if (a % b == 0)
+                return b;
+            else
+                return gcdLong(b, a % b);
+        }
+
+        public long lcmLong(long a, long b) {
+            return a * b / gcdLong(a, b);
+        }
+
+        public int sumArray(int[] a) {
+            int ans = 0;
+            for (int i = 0; i < a.length; i++) {
+                ans += a[i];
+            }
+
+            return ans;
+        }
+    }
+
+    static class ArrayUtils {
+        public ArrayUtils() {
+        }
+
+        public static int[] reverse(int[] a)
+        {
+            int n = a.length;
+            int[] b = new int[n];
+            int j = n;
+            for (int i = 0; i < n; i++) {
+                b[j - 1] = a[i];
+                j = j - 1;
+            }
+
+            return b;
         }
     }
 }
