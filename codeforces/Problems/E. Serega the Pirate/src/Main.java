9b982ecbdbf2a6fc27075b3a5b5dd726c706a744
Patrick Lin <linpatrick11@gmail.com>
2022-06-24 12:49:18 -0400

    generate 802

--- codeforces/Problems/E. Serega the Pirate/src/Main.java
+++ codeforces/Problems/E. Serega the Pirate/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,40 +10,7 @@ public class Main {
         ArrayUtils arrayUtils = new ArrayUtils();
 
         int t = sc.nextInt();
-        for (int _i = 0; _i < t; _i++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int[][] a = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    a[i][j] = sc.nextInt();
-                }
-            }
-
-            int[][] dp_max = new int[n][m];
-            int[][] dp_min = new int[n][m];
-
-            dp_max[0][0] = a[0][0];
-            dp_min[0][0] = a[0][0];
-            for (int i = 1; i < n; i++) {
-                dp_max[i][0] = dp_max[i - 1][0] + a[i][0];
-                dp_min[i][0] = dp_min[i - 1][0] + a[i][0];
-            }
-            for (int j = 1; j < m; j++) {
-                dp_max[0][j] = dp_max[0][j - 1] + a[0][j];
-                dp_min[0][j] = dp_min[0][j - 1] + a[0][j];
-            }
-            for (int i = 1; i < n; i++) {
-                for (int j = 1; j < m; j++) {
-                    dp_max[i][j] = Math.max(dp_max[i - 1][j], dp_max[i][j - 1]) + a[i][j];
-                    dp_min[i][j] = Math.min(dp_min[i - 1][j], dp_min[i][j - 1]) + a[i][j];
-                }
-            }
-
-            if(dp_max[n - 1][m - 1] % 2 == 1 || dp_min[n - 1][m - 1] > 0 || dp_max[n - 1][m - 1] < 0)
-                System.out.println("NO");
-            else
-                System.out.println("YES");
+        for(int _i = 0; _i < t; _i++) {
 
         }
     }
@@ -140,11 +106,12 @@ public class Main {
         public MathUtils() {
         }
 
-        public long gcdLong(long a, long b) {
-            if (a % b == 0)
+        public long gcdLong(long a, long b)
+        {
+            if(a%b==0)
                 return b;
             else
-                return gcdLong(b, a % b);
+                return gcdLong(b,a%b);
         }
 
         public long lcmLong(long a, long b) {
@@ -156,7 +123,8 @@ public class Main {
         public ArrayUtils() {
         }
 
-        public static int[] reverse(int[] a) {
+        public static int[] reverse(int[] a)
+        {
             int n = a.length;
             int[] b = new int[n];
             int j = n;
