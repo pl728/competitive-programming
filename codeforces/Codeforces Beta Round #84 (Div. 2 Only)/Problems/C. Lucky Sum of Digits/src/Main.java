cc7bc5329882c319ca7b73dd44de0f71a8399433
Patrick Lin <linpatrick11@gmail.com>
2022-08-07 17:27:09 -0400

    started cfladders ladder c (2/100)

---
+++ codeforces/Codeforces Beta Round #84 (Div. 2 Only)/Problems/C. Lucky Sum of Digits/src/Main.java
@@ -0,0 +1,195 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int globalVariable = 123456789;
+    static String author = "pl728 on codeforces";
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        MathUtils mathUtils = new MathUtils();
+        ArrayUtils arrayUtils = new ArrayUtils();
+
+        int n = sc.nextInt();
+        // find the smallest number containing only 4 and 7 where the sum of digits = n
+        // need 4x + 7y = n for some x, y
+        // fewer digits is always smaller, so always
+
+        // 4 - 4
+        // 7 - 7
+        // 11 - 47
+        // 15 - 447
+        // 18 - 477
+        // 19 - 4447
+        int x;
+        for(x = 7 * (n / 7); x >= 7; x -= 7) {
+            if((n - x) % 4 == 0) {
+                break;
+            }
+        }
+        if(x == 0) {
+            if(n % 4 == 0) {
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < n / 4; i++) {
+                    sb.append("4");
+                }
+                System.out.println(sb);
+            } else {
+                System.out.println(-1);
+            }
+        } else {
+            int sevens = x / 7;
+            int fours = (n - x) / 4;
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < fours; i++) {
+                sb.append("4");
+            }
+            for(int i = 0; i < sevens; i++) {
+                sb.append("7");
+            }
+            System.out.println(sb);
+        }
+    }
+
+    static class FastReader {
+        /**
+         * Uses BufferedReader and StringTokenizer for quick java I/O
+         * get next int, long, double, string
+         * get int, long, double, string arrays, both primitive and wrapped object when array contains all elements
+         * on one line, and we know the array size (n)
+         * next: gets next space separated item
+         * nextLine: returns entire line as space
+         */
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public String[] readStringArrayLine(int n) {
+            String line = "";
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return line.split(" ");
+        }
+
+        public String[] readStringArrayLines(int n) {
+            String[] result = new String[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.next();
+            }
+            return result;
+        }
+
+        // to parse something else:
+        // T x = T.parseT(fastReader.next());
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] readIntArray(int n) {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextInt();
+            }
+            return result;
+        }
+
+        public long[] readLongArray(int n) {
+            long[] result = new long[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextLong();
+            }
+            return result;
+        }
+
+        public Integer[] readIntArrayObject(int n) {
+            Integer[] result = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextInt();
+            }
+            return result;
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public char[] readCharArray(int n) {
+            return this.nextLine().toCharArray();
+        }
+
+    }
+
+    static class MathUtils {
+        public MathUtils() {
+        }
+
+        public long gcdLong(long a, long b) {
+            if (a % b == 0)
+                return b;
+            else
+                return gcdLong(b, a % b);
+        }
+
+        public long lcmLong(long a, long b) {
+            return a * b / gcdLong(a, b);
+        }
+    }
+
+    static class ArrayUtils {
+        public ArrayUtils() {
+        }
+
+        public static int[] reverse(int[] a) {
+            int n = a.length;
+            int[] b = new int[n];
+            int j = n;
+            for (int i = 0; i < n; i++) {
+                b[j - 1] = a[i];
+                j = j - 1;
+            }
+
+            return b;
+        }
+
+        public int sumArray(int[] a) {
+            int ans = 0;
+            for (int i = 0; i < a.length; i++) {
+                ans += a[i];
+            }
+
+            return ans;
+        }
+    }
+}
