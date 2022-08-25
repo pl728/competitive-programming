69d82a3461fcde1e8a586f469efac42b87458376
Patrick Lin <linpatrick11@gmail.com>
2022-08-24 23:18:13 -0400

    27/100 cfladders c

---
+++ codeforces/Codeforces Round #211 (Div. 2)/Problems/C. Fixing Typos/src/Main.java
@@ -0,0 +1,214 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int globalVariable = 123456789;
+    static String author = "pl728 on codeforces";
+    
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        MathUtils mathUtils = new MathUtils();
+        ArrayUtils arrayUtils = new ArrayUtils();
+
+        // can't have more than 2 consecutive letters
+        // can't have two consecutive followed by two consecutive
+
+        String s = sc.nextLine();
+        char[] a = new char[s.length()];
+        int[] b = new int[s.length()];
+        int x = 0;
+        int continuous = 1;
+
+        if(s.length() < 3) {
+            System.out.println(s);
+            return;
+        }
+
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == s.charAt(i - 1)) {
+                if(continuous < 2) continuous++;
+            } else {
+                a[x] = s.charAt(i - 1);
+                b[x++] = continuous;
+                continuous = 1;
+            }
+        }
+        if(s.charAt(s.length() - 2) == s.charAt(s.length() - 1)) {
+            if(continuous < 2) continuous++;
+            a[x] = s.charAt(s.length() - 1);
+            b[x] = continuous;
+        } else {
+            a[x] = s.charAt(s.length() - 1);
+            b[x] = 1;
+        }
+        x++;
+        for(int i = 1; i < x; i++) {
+            if(b[i - 1] == 2 && b[i] == 2) {
+                b[i] = 1;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < b[i]; j++) {
+                result.append(a[i]);
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    static class FastReader {
+        /**
+         * Uses BufferedReader and StringTokenizer for quick java I/O
+         * get next int, long, double, string
+         * get int, long, double, string arrays, both primitive and wrapped object when array contains all elements
+         * on one line, and we know the array size (n)
+         * next: gets next space separated item
+         * nextLine: returns entire line as space
+         */
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public String[] readStringArrayLine(int n) {
+            String line = "";
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return line.split(" ");
+        }
+
+        public String[] readStringArrayLines(int n) {
+            String[] result = new String[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.next();
+            }
+            return result;
+        }
+
+        // to parse something else:
+        // T x = T.parseT(fastReader.next());
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] readIntArray(int n) {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextInt();
+            }
+            return result;
+        }
+        
+        public long[] readLongArray(int n) {
+            long[] result = new long[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextLong();
+            }
+            return result;
+        }
+
+        public Integer[] readIntArrayObject(int n) {
+            Integer[] result = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = this.nextInt();
+            }
+            return result;
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public char[] readCharArray(int n) {
+            return this.nextLine().toCharArray();
+        }
+
+    }
+
+    static class MathUtils {
+        public MathUtils() {
+        }
+
+        public long gcdLong(long a, long b)
+        {
+            if(a%b==0)
+                return b;
+            else
+                return gcdLong(b,a%b);
+        }
+
+        public long lcmLong(long a, long b) {
+            return a * b / gcdLong(a, b);
+        }
+    }
+
+    static class ArrayUtils {
+        public ArrayUtils() {
+        }
+
+        public static int[] reverse(int[] a)
+        {
+            int n = a.length;
+            int[] b = new int[n];
+            int j = n;
+            for (int i = 0; i < n; i++) {
+                b[j - 1] = a[i];
+                j = j - 1;
+            }
+
+            return b;
+        }
+
+        public int sumIntArrayInt(int[] a) {
+            int ans = 0;
+            for (int i = 0; i < a.length; i++) {
+                ans += a[i];
+            }
+
+            return ans;
+        }
+
+        public long sumLongArrayLong(int[] a) {
+            long ans = 0;
+            for (int i = 0; i < a.length; i++) {
+                ans += a[i];
+            }
+
+            return ans;
+        }
+    }
+    
+    public static int lowercaseToIndex(char c) {
+        return (int) c - 97;
+    }
+}
